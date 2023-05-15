30d5babd05c7efa7dcaeb2c080b4a15857da9d63
alexpikuta <olexandr.pikuta@gmail.com>
2023-05-15 16:41:13 +0200

    Add scss support, Add Header component

---
+++ src/components/containers/MainHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+    <img
+      alt="Phrase logo"
+      src="@/assets/logo.svg"
+      width="109"
+      height="29"/>
+      <slot />
+  </header>
+</template>
+
+<style lang="scss">
+.header {
+  overflow: hidden;
+  background-color: $c-background-mute;
+  display: flex;
+  align-items: center;
+  padding: 0 30px;
+  gap: 20px;
+}
+</style>
---
+++ src/components/containers/MainNav.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav class="nav">
+    <ul>
+      <li
+        class="nav__item"
+        v-for="link in linkList"
+        :key="link.title">
+        <RouterLink
+          class="nav__link"
+          :to="link.route">
+          {{ link.title }}
+        </RouterLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import type { NavigationLink } from '@/types/components/Header'
+
+defineProps<{
+  linkList: NavigationLink[]
+}>()
+</script>
+
+<style lang="scss">
+.nav {
+  &__item {
+    display: inline-block;
+  }
+  &__link {
+    display: block;
+    padding: 20px 10px;
+    background: transparent;
+    font-weight: 500;
+    &:hover,
+    &:focus {
+      background-color: $c-divider-lighter-1;
+    }
+    &.router-link-active {
+      background-color: $c-divider-lighter-2;
+    }
+  }
+}
+</style>
